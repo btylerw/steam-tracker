bd14fae282ce3d2579f76bb0ab05b98ad02f2f3d
Tyler Brown <brown.tyler.w@gmail.com>
2025-07-12 15:32:56 -0700

    Separates played and unplayed games and displays them in a grid for each category

--- src/app/login/success/page.js
+++ src/app/login/success/page.js
@@ -9,13 +9,13 @@ export default function LoginSuccess() {
     const steamid = searchParams.get('steamid');
     const [profile, setProfile] = useState(null);
     const [ownedGames, setOwnedGames] = useState([]);
+    const [unplayedGames, setUnplayedGames] = useState([]);
 
     const renderGames = (games) => {
         return games.map(game => (
-            <div key={game.appid} className="flex flex-col">
-                <h3>{game.name}</h3>
-                <p>Playtime: {(game.playtime_forever / 60).toFixed(1)} hours</p>
-                <img src={`https://media.steampowered.com/steamcommunity/public/images/apps/${game.appid}/${game.img_icon_url}.jpg`} alt={`${game.name} icon`}/>
+            <div key={game.appid} className="flex">
+                <h3>{game.name} <p>Playtime: {(game.playtime_forever / 60).toFixed(1)} hours</p></h3>
+                <img className="h-24 w-24" src={`https://media.steampowered.com/steamcommunity/public/images/apps/${game.appid}/${game.img_icon_url}.jpg`} alt={`${game.name} icon`}/>
             </div>
         ))
     }
@@ -28,8 +28,11 @@ export default function LoginSuccess() {
                 const res = await axios.get(`/api/steam/profile?steamid=${steamid}`);
                 setProfile(res.data);
                 const result = await axios.get(`/api/steam/games?steamid=${steamid}`);
-                const sortedGames = result.data.sort((a, b) => b.playtime_forever - a.playtime_forever);
+                const playedGames = result.data.filter(game => game.playtime_forever > 0);
+                const sortedGames = playedGames.sort((a, b) => b.playtime_forever - a.playtime_forever);
+                const unplayedGames = result.data.filter(game => game.playtime_forever <= 0);
                 setOwnedGames(sortedGames);
+                setUnplayedGames(unplayedGames);
             } catch (err) {
                 console.error(`Error fetching profile: ${err}`);
             }
@@ -44,7 +47,14 @@ export default function LoginSuccess() {
         <div className="flex items-center justify-center flex-col">
             <h1>{profile.personaname}</h1>
             <img src={profile.avatarfull} alt="avatar" />
-            {renderGames(ownedGames)}
+            <div className="text-2xl">Played Games:</div>
+            <div className="grid grid-cols-4 gap-4">
+                {renderGames(ownedGames)}
+            </div>
+            <div className="text-2xl">Unplayed Games:</div>
+            <div className="grid grid-cols-4 gap-4">
+                {renderGames(unplayedGames)}
+            </div>
         </div>
     )
 }
