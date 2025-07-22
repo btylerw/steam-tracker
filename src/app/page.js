0b06745d427efed221b0e71b1c6e88c5e8d64adf
Tyler Brown <brown.tyler.w@gmail.com>
2025-07-21 23:20:00 -0700

    Changed steam login button to use cursor pointer

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div onClick={() => window.location.href = '/api/auth/steam'} >
-        <Image src={SteamLoginBtn} alt="Sign In To Steam"/>
+        <Image src={SteamLoginBtn} alt="Sign In To Steam" className="cursor-pointer"/>
       </div>
     </div>
   );
