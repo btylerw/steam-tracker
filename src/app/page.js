2fa1dee881f099924710ca61936be1a1085844f0
Tyler Brown <brown.tyler.w@gmail.com>
2025-08-08 00:49:44 -0700

    Properly escaped character

--- src/app/page.js
+++ src/app/page.js
@@ -29,47 +29,47 @@ export default function Home() {
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100 dark:bg-gray-900">
-  {loading ? (
-    <p className="text-xl text-gray-700 dark:text-gray-200">
-      Redirecting to Steam login...
-    </p>
-  ) : (
-    <div className="flex flex-col items-center gap-5 text-center">
-      <h1 className="text-2xl font-bold">Sign in with your Steam account</h1>
+      {loading ? (
+        <p className="text-xl text-gray-700 dark:text-gray-200">
+          Redirecting to Steam login...
+        </p>
+      ) : (
+      <div className="flex flex-col items-center gap-5 text-center">
+        <h1 className="text-2xl font-bold">Sign in with your Steam account</h1>
 
-      <p className="max-w-xl text-gray-700 dark:text-gray-300">
-        The purpose of this site is to estimate the total number of hours of
-        content worth of unfinished games in your library.
-      </p>
+        <p className="max-w-xl text-gray-700 dark:text-gray-300">
+          The purpose of this site is to estimate the total number of hours of
+          content worth of unfinished games in your library.
+        </p>
 
-      <div onClick={checkSteamSession}>
-        <Image
-          src={SteamLoginBtn}
-          alt="Sign In To Steam"
-          className="cursor-pointer"
-        />
-      </div>
-      <div className="flex flex-col sm:flex-row gap-10 mt-6">
-        <div>
-          <h2 className="text-lg font-semibold mb-2">
-            When you log in with Steam, we only collect:
-          </h2>
-          <ul className="list-disc list-inside text-left">
-            <li>Your Steam ID</li>
-            <li>Your Steam Display Name</li>
-            <li>Your owned games and playtime data</li>
-          </ul>
+        <div onClick={checkSteamSession}>
+          <Image
+            src={SteamLoginBtn}
+            alt="Sign In To Steam"
+            className="cursor-pointer"
+          />
         </div>
+        <div className="flex flex-col sm:flex-row gap-10 mt-6">
+          <div>
+            <h2 className="text-lg font-semibold mb-2">
+              When you log in with Steam, we only collect:
+            </h2>
+            <ul className="list-disc list-inside text-left">
+              <li>Your Steam ID</li>
+              <li>Your Steam Display Name</li>
+              <li>Your owned games and playtime data</li>
+            </ul>
+          </div>
 
-        <div>
-          <h2 className="text-lg font-semibold mb-2">We don't:</h2>
-          <ul className="list-disc list-inside text-left">
-            <li>Store your Steam account name</li>
-            <li>Store your Steam password</li>
-            <li>Sell or share your data</li>
-          </ul>
+          <div>
+            <h2 className="text-lg font-semibold mb-2">We don&apos;t:</h2>
+            <ul className="list-disc list-inside text-left">
+              <li>Store your Steam account name</li>
+              <li>Store your Steam password</li>
+              <li>Sell or share your data</li>
+            </ul>
+          </div>
         </div>
-      </div>
     </div>
   )}
 </div>
